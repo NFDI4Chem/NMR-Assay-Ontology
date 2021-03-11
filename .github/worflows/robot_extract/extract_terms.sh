0f2e3c5f7d55f3b51bd2f20990663a06df51c125
Castro0o <andre@andrecastro>
2021-03-11 09:16:02 +0100

    action added: robot extract

---
+++ .github/worflows/robot_extract/extract_terms.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+while IFS=" " read -r prefix uri 
+do
+    # echo "prefix:" $prefix "uri:" $uri $prefix.owl
+
+    #### extract terms from ontologies
+    if test -f "terms_$prefix.txt"; then
+        echo "extracting terms from $prefix terms_$prefix.txt"
+        robot extract \
+        --method BOT \
+        --input $prefix.owl \
+        --term-file terms_$prefix.txt \
+        --intermediates none \
+        --annotate-with-source true \
+        --output extracted_$prefix.owl
+
+        # convert owl to ttl and remove owl
+        robot convert --input extracted_$prefix.owl \
+        --output extracted_$prefix.ttl
+    fi
+done < "ontologies.txt"
+
+# Merge extracted_*.owl on to merged.owl
+robot merge --inputs "extracted_*.owl" --output merged.owl
+robot convert --input merged.owl --output merged.ttl 
+rm extracted_*.owl  # remove owl files
+
+
+
+# for o in `cat ontologies.txt`
+# do
+#     o_l=($o)
+#     o_line=${o_l[0]}
+#     echo 'line' $o_line
+
+#     o_array=($o_line)
+#     echo "one" ${o_array[0]}
+#     # o_prefix=${o_array[0]}
+#     # o_uri=${o_array[1]}
+#     # echo $o_prefix
+#     # echo uri $o_uri
+#     # wget $o_uri -c 
+# done
